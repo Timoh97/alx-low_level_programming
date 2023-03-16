d3c81ea3088859970ad7107e207cea4789401194
Timoh97 <timohmugendi@gmail.com>
2023-03-16 15:47:19 -0700

    hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Return:1
+ */
+
+int main(void)
+{
+
+	fprintf(stderr,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+			return (1);
+
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -5,4 +5,5 @@
 5. Hello, puts
 6. Hello, printf
 7. Size is not grandeur, and territory does not make a nation
-
+8. Intel 
+9. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity 
