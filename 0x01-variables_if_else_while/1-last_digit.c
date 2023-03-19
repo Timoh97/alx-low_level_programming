492b58aa92d72adb8ff76e6b480934cb28524ab4
Timoh97 <timohmugendi@gmail.com>
2023-03-19 08:20:11 -0700

    change var declaration

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,26 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <time.h>
+#include <stdio.h>
+
 /**
- * main -main code block
- * Description: assign a random number to the variable n
- * each time it is executed
- * Return:0
+ * main - Entry point of program
+ *
+ * Return: 0 (success)
  */
 int main(void)
-
 {
 	int n;
-	int last;
+	int lnum;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	lnum = n % 10;
-
-	if (lnum > 5)
-		printf("Last digit of %i is %i and is greater than 5\n", n, lnum);
+	if (lnum < 6 && lnum != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+		n, lnum);
+	else if (lnum > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, lnum);
 	else if (lnum == 0)
-		printf("Last digit of %i is %i and is 0\n", n, lnum);
-	else if (lnum < 6)
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, lnum);
-
+		printf("Last digit of %d is %d and is 0\n", n, lnum);
 	return (0);
-
 }
