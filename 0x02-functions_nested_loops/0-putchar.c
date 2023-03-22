c2a735b87e09466c6f6aa375d927b97e1c26e657
Timoh97 <timohmugendi@gmail.com>
2023-03-22 12:36:56 -0700

    create .c files for various tasks

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: (0). Success
+ */
+
+int main(void)
+{
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar(10);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase followed by a new line.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+
+{
+	int lower = 97
+
+	while (lower < 123)
+		_putchar(lower);
+		lower++
+	_putchar(\n);
+
+return (0);
+}
