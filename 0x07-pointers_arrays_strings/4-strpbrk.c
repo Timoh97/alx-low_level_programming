a6b082aec62a75aa14da5cd583ee23e4628cc371
Timoh97 <timohmugendi@gmail.com>
2023-04-04 19:05:38 -0700

    4. strpbrk and 3. strspn

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+		int index;
+
+		while (*s)
+		{
+			for (index = 0; accept[index]; index++)
+			{
+			if (*s == accept[index])
+			return (s);
+			}
+		s++;
+		}
+
+	return ('\0');
+}
