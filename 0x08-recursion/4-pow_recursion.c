72870b6c5ac387152e6e2a7cd44d43e504a88053
Timoh97 <timohmugendi@gmail.com>
2023-04-06 14:42:39 -0700

    power recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * factorial - returns the factorial of a given number
- * @n: integer to take factorial of
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: base number
+ * @y: exponent
  *
- * Return: factorial of n, or -1 if n < 0
+ * Return: x ^ y or -1 if y < 0
  */
 int _pow_recursion(int x, int y)
 {
