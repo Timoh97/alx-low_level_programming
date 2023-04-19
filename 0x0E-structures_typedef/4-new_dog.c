9a6101c132ee1292f133c9ca2831eb4a944a39f7
Timoh97 <timohmugendi@gmail.com>
2023-04-19 01:34:45 -0700

    Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to re

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,14 +9,14 @@
  *
  * Return: pointer to new dog
  */
-dog_tim *new_dog(char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	unsigned int k, l, m;
-	dog_tim *dog;
+	dog_t *dog;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-	dog = malloc(sizeof(dog_tim));
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 	for (k = 0; name[k]; k++)
